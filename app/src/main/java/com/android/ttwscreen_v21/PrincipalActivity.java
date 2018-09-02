77eb70b6c260183567e79baf550dcbbe34aff07d
magnomalkut <mgnmiguel@gmail.com>
2018-09-02 05:35:04 -0300

    Botão Finalizar

---
+++ app/src/main/java/com/android/ttwscreen_v21/PrincipalActivity.java
@@ -0,0 +1,32 @@
+package com.android.ttwscreen_v21;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class PrincipalActivity extends Activity implements AdapterView.OnItemSelectedListener {
+    Spinner spinner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+        spinner = findViewById(R.id.spinPatio);
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this,R.array.patio, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
